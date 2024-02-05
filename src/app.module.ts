7a9f0055ac3783c314a40dfacf2cc4d9b4565c3c
JJRoca <hanzoblade1234@outlook.com>
2024-02-05 00:33:31 -0400

    e

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,7 @@ import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://roca:mo3HJFShzMntb5F0@cluster0.5pagdww.mongodb.net/fruitable',
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
     CategoryModule,
     ProductModule,
